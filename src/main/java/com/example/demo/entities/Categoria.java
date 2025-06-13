636aafffab4fef6f422ee276e88bef3a7db291e0
XxemicoxX <emiliojc2004@gmail.com>
2025-06-13 13:36:52 -0500

    13/06/25-13:36

--- src/main/java/com/example/demo/entities/Categoria.java
+++ src/main/java/com/example/demo/entities/Categoria.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,15 +16,9 @@ import lombok.NoArgsConstructor;
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
     @Column(name = "id_categoria")
     private Integer id;
     @Column(name = "nombre_categoria")
     private String nombre;
     private String descripcion;
-=======
-    private Integer id_categoria;
-    @NotBlank(message = "Eliga la categoria coreespondiente")
-    private String nombre;
->>>>>>> 2336c3570793558e31cb2df23d9277932d9e7ae8
 }
--- src/main/java/com/example/demo/entities/Tienda.java
+++ src/main/java/com/example/demo/entities/Tienda.java
@@ -18,16 +18,11 @@ import lombok.NoArgsConstructor;
 public class Tienda {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-<<<<<<< HEAD
     @Column(name = "id_tienda")
     private Integer id;
-    private String nombre;
-=======
-    private Integer id_tienda;
     @NotBlank(message = "El nombre es un campo obligatorio")
     private String nombre;
     @NotBlank(message = "El direccion es un campo obligatorio")
     @Size(min = 10, max = 100,message = "La direccion debe de tener entre 10 a 100 caracterres")
->>>>>>> 2336c3570793558e31cb2df23d9277932d9e7ae8
     private String direccion;
 }
