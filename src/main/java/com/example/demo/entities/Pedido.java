e50cbc8bf572f92a4b1aeb1307f24db5172e602a
XxemicoxX <emiliojc2004@gmail.com>
2025-06-13 22:14:03 -0500

    13/06/25-22:13

--- src/main/java/com/example/demo/entities/Pedido.java
+++ src/main/java/com/example/demo/entities/Pedido.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +17,10 @@ public class Pedido {
     @Column(name = "id_pedido")
     private Integer id;
     private LocalDate fecha;
+    @ManyToOne
     @Column(name = "id_usuario")
     private Usuario usuario;
+    @ManyToOne
     @Column(name = "id_tienda")
     private Tienda tienda;
 }
